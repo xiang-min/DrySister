aeb8bdb966d7dc07e95b0275ba4cebaa41bd4196
xiangmincode <980003162@qq.com>
2019-09-17 17:52:26 +0800

    first commit

---
+++ app/src/main/java/com/example/minxiang/drysister/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.minxiang.drysister;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
